00c6fb7c9311f24d37032b695eb9eaf8952b786b
Guilherme Costa Sales <guilhermecosales@gmail.com>
2023-02-02 11:32:46 -0300

    feat: implement the product repository

---
+++ src/main/java/com/github/productapi/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.github.productapi.repository;
+
+import com.github.productapi.entity.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+
+}
