253be02be6b92248cd6cb3440432a91ba68f33a5
Guilherme Costa Sales <guilhermecosales@gmail.com>
2023-02-02 11:35:01 -0300

    feat: add empty constructor

--- src/main/java/com/github/productapi/entity/Product.java
+++ src/main/java/com/github/productapi/entity/Product.java
@@ -3,12 +3,14 @@ package com.github.productapi.entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Builder
 @Document
+@NoArgsConstructor
 @AllArgsConstructor
 public class Product {
 
